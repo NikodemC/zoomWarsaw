8b7c5af163f78ef2aa0820ade9b8602251aa5149
NikodemC <46518251+NikodemC@users.noreply.github.com>
2019-09-11 13:51:54 +0200

    Add project files.

---
+++ ZooM.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZooM.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ZooM.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZooM.Core
+{
+    public class Class1
+    {
+    }
+}
