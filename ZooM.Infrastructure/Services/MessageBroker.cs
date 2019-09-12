c1b5aa3478093beb5ecdc9d2b141c24934c10cea
NikodemC <nikodem321@hotmail.com>
2019-09-12 10:00:25 +0200

    Added rabbitMq infrastructure

--- ZooM.Infrastructure/Services/MessageBroker.cs
+++ ZooM.Infrastructure/Services/MessageBroker.cs
@@ -1,14 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using ZooM.Application.Events;
 using ZooM.Application.Services;
+using ZooM.Infrastructure.RabbitMq;
+using ZooM.Infrastructure.RabbitMq.CQRS;
 
 namespace ZooM.Infrastructure.Services
 {
     internal sealed class MessageBroker : IMessageBroker
     {
+        private readonly IMessagePublisher _publisher;
+
+        public MessageBroker(IMessagePublisher publisher)
+            => _publisher = publisher;
+
         public async Task PublishAsync(params IEvent[] events)
         {
-            await Task.Delay(1);
+            var tasks = events.Select(e => _publisher.PublishEventAsync(e));
+            await Task.WhenAll(tasks);
         }
     }
 }
